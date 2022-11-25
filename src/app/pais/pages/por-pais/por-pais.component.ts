3ceefbd66ef929f9708ed56f5efcf56c9dd9a86e
JuanCGarciaA <juancarlos@develop.mx>
2022-11-25 15:07:46 -0600

    Finalización de todo el proyecto

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,7 +6,11 @@ import { Country } from '../../interfaces/pais.interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [
+  styles: [`
+  li {
+    cursor: pointer;
+  }
+  `
   ]
 })
 export class PorPaisComponent {
@@ -15,9 +19,13 @@ export class PorPaisComponent {
   hayError: boolean = false;
   paises: Country[] = [];
 
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
+
   constructor( private paisService: PaisService) { }
 
   buscar( termino: string){
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
 
@@ -33,6 +41,20 @@ export class PorPaisComponent {
 
   sugerencias( termino: string){
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+
+    this.paisService.buscarPais( termino )
+      .subscribe( paises => this.paisesSugeridos = paises.splice(0,5) 
+      , (err) => this.paisesSugeridos = []
+      );
+
+  }
+
+  buscarSugerido ( termino: string){
+    this.buscar( termino );
+    
+
 
   }
 
